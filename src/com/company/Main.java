197ae8b05346de63c8748680abad65f485df04eb
eg913 <eg913@pvsd.org>
2018-10-22 11:21:25 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String names [] = {"Emma", "Liam", "Olivia", "Noah", "Ava",	"Oliver", "Isabella", "Mason", "Sophia",
+                "Lucas", "Amelia", "Elijah", "Mia", "Logan", "Charlotte", "Ethan", "Harper", "James", "Mila",
+                "Aiden", "Aria", "Jackson", "Avery", "Carter", "Ella", "Sebastian", "Evelyn", "Alexander", "Luna",
+                "Benjamin", "Sofia", "Jacob", "Abigail","Michael", "Layla", "William", "Scarlett", "Daniel",
+                "Riley", "Grayson", "Ellie", "Jack", "Emily", "Leo", "Lily", "Luke", "Chloe", "Henry", "Madison",
+                "Jayden"};
+        names.sort();
+
+
+
+
+    }
+}
